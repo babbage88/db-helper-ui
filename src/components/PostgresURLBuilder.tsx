da8f078e4bac31b067baf2d428c1a80b26d9f73a
Justin Trahan <justin@trahan.dev>
2025-05-05 19:05:46 -0500

    unused imports

--- src/components/PostgresURLBuilder.tsx
+++ src/components/PostgresURLBuilder.tsx
@@ -1,6 +1,6 @@
 // src/components/PostgresUrlBuilder.tsx
 
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
