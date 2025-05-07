348792ff82b02ce26b5574c90e4e231aa02f235e
Justin Trahan <justin.m.trahan@outlook.com>
2025-05-07 14:21:21 +0000

    test-envvite

--- src/lib/db-helper-svc.ts
+++ src/lib/db-helper-svc.ts
@@ -1,44 +1,45 @@
 // src/lib/downloadZip.ts
+const baseUrl = import.meta.env.VITE_API_BASE_URL
 
 export const downloadZip = async (
-    dbHostname: string,
-    dbPort: number,
-    superuserUsername: string,
-    superuserPassword: string,
-    serviceUsername: string,
-    servicePassword: string,
-    databaseName: string
-  ) => {
-    try {
-      //const baseUrl = import.meta.env.VITE_API_BASE_URL
-      const res = await fetch(`/api/download-pg-scripts`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          dbHostname,
-          dbPort,
-          superuserUsername,
-          superuserPassword,
-          serviceUsername,
-          servicePassword,
-          databaseName,
-        }),
-      })
-  
-      if (!res.ok) {
-        throw new Error("Failed to download ZIP file")
-      }
-  
-      const blob = await res.blob()
-      const url = window.URL.createObjectURL(blob)
-      const a = document.createElement("a")
-      a.href = url
-      a.download = "pg_setup_scripts.zip"
-      a.click()
-      window.URL.revokeObjectURL(url)
-    } catch (err) {
-      console.error(err)
-      alert("An error occurred while downloading the ZIP.")
+  dbHostname: string,
+  dbPort: number,
+  superuserUsername: string,
+  superuserPassword: string,
+  serviceUsername: string,
+  servicePassword: string,
+  databaseName: string
+) => {
+  try {
+    console.log(`${baseUrl}/api/download-pg-scripts`)
+    const res = await fetch(`/api/download-pg-scripts`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        dbHostname,
+        dbPort,
+        superuserUsername,
+        superuserPassword,
+        serviceUsername,
+        servicePassword,
+        databaseName,
+      }),
+    })
+
+    if (!res.ok) {
+      throw new Error("Failed to download ZIP file")
     }
+
+    const blob = await res.blob()
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement("a")
+    a.href = url
+    a.download = "pg_setup_scripts.zip"
+    a.click()
+    window.URL.revokeObjectURL(url)
+  } catch (err) {
+    console.error(err)
+    alert("An error occurred while downloading the ZIP.")
   }
-  
+}
+
