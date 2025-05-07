32471e1d63a6ded61fd028d55467f7ed28efcbed
Justin Trahan <justin.m.trahan@outlook.com>
2025-05-07 19:57:56 +0000

    checkpoint-lb

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,24 @@ import { DbUserSetup } from "./components/DbUserSetup"
 import { ThemeProvider } from "./components/ui/theme-provider"
 import { ModeToggle } from "./components/ui/mode-toggle"
 import { Toaster } from "@/components/ui/sonner"
+import { NavMenu } from "@/components/db-helper/NavMenu"
 
 
 export default function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle></ModeToggle>
-      <Toaster />
-      <Router>
-        <Routes>
-          <Route path="/" element={<PostgresUrlBuilder />} />
-          <Route path="/pgurlbuilder" element={<PostgresUrlBuilder />} />
-          <Route path="/dbusersetup" element={<DbUserSetup />} />
-          <Route path="/scripts" element={<DbUserSetup />} />
-        </Routes>
-      </Router>
+        <ModeToggle></ModeToggle>
+        <NavMenu></NavMenu>
+        <Toaster />
+        <Router>
+          <Routes>
+            <Route path="/" element={<PostgresUrlBuilder />} />
+            <Route path="/pgurlbuilder" element={<PostgresUrlBuilder />} />
+            <Route path="/dbusersetup" element={<DbUserSetup />} />
+            <Route path="/scripts" element={<DbUserSetup />} />
+          </Routes>
+        </Router>
       </ThemeProvider>
     </>
 
---
+++ src/components/db-helper/NavMenu.tsx
@@ -0,0 +1,105 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
+
+
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "Db URL Builder",
+    href: "/pgurlbuilder",
+    description:
+      "Generate and validate a database URL or ConnectionString",
+  },
+  {
+    title: "Dev Db Builder",
+    href: "/scripts",
+    description:
+      "Generate scripts that will create a new application database and user. Setup permissions for new user.",
+  },
+]
+
+export function NavMenu() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      DbBob
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      <br>No need to search for "Postgres 17 create new database and grant access to user" or ask AI </br>
+                      <br>"ive granted all Piviliges to my new app user, why am I still getting this connection error in my golang backend?" </br>
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Useful utilities for spinning up and managing development databases.
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Utilities</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
+
