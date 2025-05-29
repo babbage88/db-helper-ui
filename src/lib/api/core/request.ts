41427e000c160c0738ce3bfe16bfd5c71062ad04
Justin Trahan <justin@trahan.dev>
2025-05-28 20:51:52 -0500

    testing axios refresh

--- src/lib/api/core/request.ts
+++ src/lib/api/core/request.ts
@@ -1,10 +1,6 @@
 // src/lib/api/core/request.ts
 import type { ApiRequestOptions } from './ApiRequestOptions';
-import type { ApiResult } from './ApiResult';
 import { axiosInstance } from '../axiosInstance'; // your axios with interceptors
-import type { AxiosRequestConfig } from 'axios';
-
-
 
 export async function request(options: ApiRequestOptions): Promise<any> {
   const { method, url, headers = {}, body, mediaType } = options;
