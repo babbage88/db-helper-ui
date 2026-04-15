81bece00d3ab37209c7ae6195014edcabd6abed4
Justin Trahan <justin@trahan.dev>
2026-04-15 06:10:56 -0500

    implemented manualChunking in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
+import { visualizer } from "rollup-plugin-visualizer";
 import react from '@vitejs/plugin-react'
 import fs from 'fs';
 
@@ -8,7 +9,57 @@ import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('node_modules/react/') ||
+              id.includes('node_modules/react-dom/') ||
+              id.includes('node_modules/react-router/') ||
+              id.includes('node_modules/react-router-dom/')
+            ) {
+              return '@framework-core';
+            }
+            if (id.includes('react-select')) {
+              return 'react-select';
+            }
+            if (id.includes('react-hook-form')) {
+              return 'react-hook-form';
+            }
+            if (id.includes('zod')) {
+              return 'zod';
+            }
+            if (id.includes('tailwind-merge')) {
+              return 'tailwind-merge';
+            }
+            if (id.includes('xterm')) {
+              return 'xterm';
+            }
+            if (id.includes('axios')) {
+              return 'axios';
+            }
+            if (id.includes('tanstack')) {
+              return 'tanstack';
+            }
+            if (id.includes('sonner')) {
+              return 'sonner';
+            }
+            if (id.includes('date-fns')) {
+              return 'date-fns';
+            }
+            if (id.includes('radix-ui')) {
+              return 'radix-ui';
+            }
+          }
+        
+        }
+      }
+    }
+  },
+  plugins: [react(), tailwindcss(), visualizer({ open: false, filename: "bundle-analysis.html" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
